7d684fe0110419c6335b25b0362ac8e204fa3624
jonathan <koren@pukogames.com>
2021-12-22 15:57:37 +0900

    Fix script import using pgfile

--- script.sh
+++ script.sh
@@ -5,9 +5,12 @@ filename=$(date +'%Y%m%d-%H%M')-$RANDOM.gz
 echo "Exporting PostgreSQL data as $filename"
 
 if [ "$PGPASS" != "" ]; then
-  pg_dump -f $filename -Z 5 -U $PGUSER -W $PGPASS
+  echo "*:*:$PGDATABASE:$PGUSER:$PGPASS" > ~/.pgpass
+  chmod 600 ~/.pgpass
+  export PGPASSFILE=~/.pgpass
+  pg_dump -f /backups/$filename -Z 5
 else
-  pg_dump -f $filename -Z 5 -U $PGUSER --no-password
+  pg_dump -f /backups/$filename -Z 5 -U $PGUSER --no-password
 fi
 
 echo "PostgreSQL data exported, uploading $filename to GCS (gs://${GCS_BUCKET})"
